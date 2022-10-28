0f94f6a1208af361d4f355ca5aae1754b0368388
Titiloye Temitope Michael <titimichael01@email.com>
2022-10-28 09:12:50 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const { urlencoded } = require('express')
+const express= require('express')
+const cors = require("cors")
+
+const app = express()
+const port = process.env.PORT || 8000;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(cors({
+    origin:"*"
+}));
+
+console.log ("ok")
+
+app.get("/", (req, res) => {
+    res.json({ 
+        "slackUsername": "Titimike1", 
+        "backend": "True", 
+        "age": 18, 
+        "bio": "My name is Titiloye Temitope Michael, I'm a Web Developer" 
+    })
+    })
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+  
